e5211b9fffb74683f1b8e927d6169498f56d3f61
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-03-14 03:05:46 +0900

    ストア作成処理を実装

--- indexed_db_test/src/modules/UserStore.js
+++ indexed_db_test/src/modules/UserStore.js
@@ -1,5 +1,9 @@
 // DB名
 const dbName = "users";
+// DBバージョン
+const dbVersion = 1;
+// Store名
+const storeName = "userStore";
 
 /**
  * ユーザストアクラス
@@ -10,16 +14,28 @@ class UserStore {
      */
     constructor() {
         this.conn = null;
+        this.db = null;
     }
 
     /**
      * 開く
      */
     open() {
-        this.conn = indexedDB.open(dbName);
+        this.conn = indexedDB.open(dbName, dbVersion);
+
+        // エラー発生
         this.conn.onerror = (error) => {  
             Promise.reject(error);
         };
+
+        // DB更新
+        // ※バージョンを指定して開いた時、新しいバージョンが指定されていた場合も通る
+        const self = this;
+        this.conn.onupgradeneeded = (event) => {
+            self.db = event.target.result;
+
+            self.db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
+        }
     }
 
     /**
@@ -27,6 +43,8 @@ class UserStore {
      */
     close() {
         if (!this.conn) { return; }
+        
+        this.db = null;
         
         this.conn.close();
         this.conn = null;
