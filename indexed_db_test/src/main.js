7a6a58d3c3f91d700d114a772e3ecd20d64aef62
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-03-14 04:13:04 +0900

    ValidationObserverを使えるようにした

--- indexed_db_test/src/main.js
+++ indexed_db_test/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { ValidationProvider, extend } from 'vee-validate';
+import { ValidationObserver, ValidationProvider, extend } from 'vee-validate';
 import { required } from 'vee-validate/dist/rules';
 
+Vue.component('ValidationObserver', ValidationObserver);
 Vue.component('ValidationProvider', ValidationProvider);
 extend('required', required);
 
