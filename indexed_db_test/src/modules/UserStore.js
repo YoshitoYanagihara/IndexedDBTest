2ecfe59ce5bf0fe29940de522f293d51f73903eb
YoshitoYanagihara <yos_yanagihara@yahoo.co.jp>
2022-03-14 02:56:55 +0900

    DBのオープン・クローズ処理を実装

--- indexed_db_test/src/modules/UserStore.js
+++ indexed_db_test/src/modules/UserStore.js
@@ -1,7 +1,35 @@
-// ユーザストアクラス
+// DB名
+const dbName = "users";
+
+/**
+ * ユーザストアクラス
+ */
 class UserStore {
-    // コンストラクタ
+    /**
+     * コンストラクタ
+     */
     constructor() {
+        this.conn = null;
+    }
+
+    /**
+     * 開く
+     */
+    open() {
+        this.conn = indexedDB.open(dbName);
+        this.conn.onerror = (error) => {  
+            Promise.reject(error);
+        };
+    }
+
+    /**
+     * 閉じる
+     */
+    close() {
+        if (!this.conn) { return; }
+        
+        this.conn.close();
+        this.conn = null;
     }
 }
 
